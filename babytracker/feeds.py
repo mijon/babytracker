8be5ddbbe9c986ac0e59ddff11ee621aa20cedbf
Michael Jones <michael.jones.gsy@gmail.com>
2025-03-30 11:42:37 +0100

    Simplfied writing feeds to db

--- babytracker/feeds.py
+++ babytracker/feeds.py
@@ -5,28 +5,27 @@ from datetime import (datetime, timedelta)
 bp = Blueprint("feeds", __name__, url_prefix="/feeds")
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def index():
     db = get_db()
-    feeds = db.execute(
+
+    if request.method == "POST":
+        feed_end = datetime.now()
+        feed_duration = timedelta(hours=int(request.form["hour"]),
+                                  minutes=int(request.form["minute"]),
+                                  seconds=int(request.form["second"]),
+                                  microseconds=int(request.form["count"]))
+        feed_start = feed_end - feed_duration
+        breast = request.form["breast"]
+        db.execute("INSERT INTO feeds (start_time, stop_time, breast, duration)"
+                   " VALUES (?, ?, ?, ?)",
+                   (feed_start, feed_end, breast, str(feed_duration)))
+        db.commit()
+
+        return redirect(url_for("feeds.index"))
+
+    feedlog = db.execute(
         "SELECT start_time, stop_time, duration, breast FROM feeds ORDER BY id DESC"
     ).fetchall()
-    return render_template("feeds/index.html", feeds=feeds)
 
-
-@bp.route("/submit", methods=["POST"])
-def submit():
-    db = get_db()
-    feed_end = datetime.now()
-    feed_duration = timedelta(hours=int(request.form["hour"]),
-                              minutes=int(request.form["minute"]),
-                              seconds=int(request.form["second"]),
-                              microseconds=int(request.form["count"]))
-    feed_start = feed_end - feed_duration
-    breast = request.form["breast"]
-    db.execute("INSERT INTO feeds (start_time, stop_time, breast, duration)"
-               " VALUES (?, ?, ?, ?)",
-               (feed_start, feed_end, breast, str(feed_duration)))
-    db.commit()
-
-    return redirect(url_for("feeds.index"))
+    return render_template("feeds/index.html", feedlog=feedlog)
